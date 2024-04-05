68a6c0277f335c8b5023afb9126aba8c970655f9
Simon Black <simonblack1@hotmail.com>
2024-04-05 11:30:32 +0100

    Removing documents and updating the script to execute main
    Removing documents and updating the script to execute main

--- script.ts
+++ script.ts
@@ -1,8 +1,8 @@
 import { MendixPlatformClient} from "mendixplatformsdk";
 import { ModelSdkClient, IModel, projects, domainmodels, microflows, pages, navigation, texts, security, IStructure, menus } from "mendixmodelsdk";
 
-const appID = "{{appID}}";
-const projectName = "{{ProjectName}}";
+const appID = "fcc8a372-b054-4cb8-876c-ba4beeb7b41b";
+const projectName = "Mendix Experience";
 const revNo = -1; // -1 for latest
 const branchName = null // null for mainline
 const wc = null;
@@ -87,3 +87,4 @@ async function main() {
         }
         return "";
     }
+main().catch(console.error);
